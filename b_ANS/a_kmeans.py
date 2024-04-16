a8070f2aa2388083eebd83724cb746f99cd91308
juancamiloespana <camilo1260@gmail.com>
2024-04-16 17:54:08 -0500

    actualización

--- b_ANS/a_kmeans.py
+++ b_ANS/a_kmeans.py
@@ -8,11 +8,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import silhouette_score ## indicador para consistencia de clúster
 from kneed import KneeLocator ### para detectar analíticamente el cambio en la pendiente
 
-from clusteval import clusteval ### para detecter numero de cluster automáticamente
-
-
+#from clusteval import clusteval ### para detecter numero de cluster automáticamente
 ### verificar nulos
-### imputar
+### imputar/eliminar
 ### escalar 
 
 df_iris = sns.load_dataset('iris')
@@ -23,6 +21,18 @@ features=df_iris[['sepal_length','sepal_width']]
 sc= StandardScaler().fit(features)
 features_sc=sc.transform(features)
 
+###k = 3
+
+
+kmeans=cluster.KMeans(n_clusters=3, n_init=10)
+kmeans.fit(features_sc)
+
+cluster_label=kmeans.labels_
+centroides= kmeans.cluster_centers_
+
+sns.scatterplot(x='sepal_length', y='sepal_width', hue=cluster_label, data=df_iris, palette='viridis')
+
+kmeans.inertia_
 
 
 
